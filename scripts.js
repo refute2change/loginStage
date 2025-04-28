4f843582aaef93606b2f5885ec63866fbe56a8a2
refute2change <madguyinthehouse@gmail.com>
2025-04-28 21:00:15 +0700

    simple login page

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function handleLogin() {
+    const password = document.getElementById('password').value;
+
+    window.location.replace("https://stackoverflow.com");
+    event.preventDefault();
+}
